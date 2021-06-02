eb3e368ee865b8a37eff1fd9b990deae3dedcbad
Dave Kidd <david.kidd@lambdaschool.com>
2021-06-01 19:15:16 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -42,6 +42,17 @@ server.get("/api/dogs", (req,res)=>{
 })
 
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
+server.post("/api/dogs", (req,res)=>{
+    const newDog = req.body
+    Dog.create(newDog)
+        .then(dog=>{
+            res.json(dog)
+        })
+        .catch(err=>{
+            res.status(500).json({message:err.message})
+        })
+
+})
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
