141dc039e8c6a57233a5b11eadbc2e9109102049
Dave Kidd <david.kidd@lambdaschool.com>
2021-06-01 18:57:40 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -27,7 +27,6 @@ server.get("/api/dogs", (req,res)=>{
         .catch(err=>{
             res.status(500).json({message:err.message})
         })
-
 })
 
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
