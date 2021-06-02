514a015f9278ad47f9e3287881131af09d4dc8e7
Dave Kidd <david.kidd@lambdaschool.com>
2021-06-01 19:25:43 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -64,7 +64,7 @@ server.put("/api/dogs/:id", async (req,res)=>{
         const updatedDog = await Dog.update(id,changes)
         res.status(200).json(updatedDog)
     }catch(err){
-
+        res.status(500).json({message:err.message})
     }
 })
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
