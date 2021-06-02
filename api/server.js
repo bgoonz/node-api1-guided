5d657844fdcc19337b09fc1bc0e5cfc9679c0585
Dave Kidd <david.kidd@lambdaschool.com>
2021-06-01 19:39:15 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -78,6 +78,7 @@ server.put("/api/dogs/:id", async (req,res)=>{
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 server.delete("/api/dogs/:id", async (req,res)=>{
     try{
+        throw "holy crap!"
         const {id} = req.params
         const deletedDog = await Dog.delete(id)
         if(!deletedDog){
@@ -86,7 +87,7 @@ server.delete("/api/dogs/:id", async (req,res)=>{
             res.status(200).json(deletedDog)
         }        
     }catch(err){
-        res.status(500).json({message:err.message})
+        res.status(500).json({message:err})
     }
 })
 // [GET] / (Hello World endpoint)
