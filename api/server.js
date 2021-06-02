6359eb3d9b6bc814630928418c3084daf41a2131
Dave Kidd <david.kidd@lambdaschool.com>
2021-06-01 19:09:57 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -18,7 +18,11 @@ server.get("/api/dogs/:id",(req,res)=>{
     const idVar = req.params.id
     Dog.findById(idVar)
         .then(dog =>{
-            res.json(dog)
+            if(!dog){
+                res.status(404).json("Dog doesn't exist")
+            }else{
+                res.json(dog)
+            }            
         })
         .catch(err=>{
             res.status(500).json({message:err.message})
